6ce6d74b30edd65575c328ca0ca9d9d57e5638b4
Sergio Luna <s96luna@gmail.com>
2016-10-06 15:23:21 -0700

    Added Splash Screen, goes to Welcome.class

---
+++ app/src/main/java/cse110/liveasy/Splash.java
@@ -0,0 +1,28 @@
+package cse110.liveasy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    /** Duration of Splash **/
+    private final int SPLASH_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent openWelcome = new Intent(Splash.this, Welcome.class);
+                Splash.this.startActivity(openWelcome);
+                Splash.this.finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
