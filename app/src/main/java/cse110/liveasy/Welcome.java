17d558e17754faeb9c47cf9542641e5f16a6f0cf
tarekgouda <targouda@gmail.com>
2016-10-09 14:02:50 -0700

    Initial Welcome Screen with buttons and logo

--- app/src/main/java/cse110/liveasy/Welcome.java
+++ app/src/main/java/cse110/liveasy/Welcome.java
@@ -2,6 +2,7 @@ package cse110.liveasy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.drawable.shapes.OvalShape;
 
 public class Welcome extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
     }
 }
