b078719e77186e405f4f574c7615a28d38c54f8c
Gilberto Acevedo <gilbert.acevedo.g@gmail.com>
2016-10-23 00:46:52 -0700

    Updated Login and Sign in, Saved user info to database

---
+++ app/src/main/java/cse110/liveasy/User.java
@@ -0,0 +1,21 @@
+package cse110.liveasy;
+
+/**
+ * Created by gilbe_000 on 10/22/2016.
+ */
+
+public class User {
+    public String username;
+    public String full_name;
+    public String phone_number;
+    public String email;
+
+
+    public User(String full_name, String phone_number, String email) {
+        this.username = username;
+        this.full_name = full_name;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+
+}
