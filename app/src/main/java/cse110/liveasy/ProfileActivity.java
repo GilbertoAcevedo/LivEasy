1774b4d58bd0eb6a1ffdc9ad82167d742f5db682
tarekgouda <targouda@gmail.com>
2016-11-06 21:25:02 -0800

    this is for editing profile, phone number completed

--- app/src/main/java/cse110/liveasy/ProfileActivity.java
+++ app/src/main/java/cse110/liveasy/ProfileActivity.java
@@ -1,19 +1,26 @@
 package cse110.liveasy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static cse110.liveasy.R.id.tvNumber1;
+import static cse110.liveasy.R.id.tvNumber3;
 
 public class ProfileActivity extends AppCompatActivity {
 
     Bundle extras;
+    String originalPhoneNumber;
+    String changedPhoneNumber;
+    String originalEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,72 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setTitle((String)extras.get("username"));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        final TextView phoneNumber = (TextView) findViewById(tvNumber1);
+        final EditText editphoneNumber = (EditText) findViewById(R.id.phone_edit_text);
+
+        final TextView email  = (TextView) findViewById(tvNumber3);
+        final EditText editEmail = (EditText) findViewById(R.id.email_edit);
+
+        originalPhoneNumber = phoneNumber.getText().toString();
+        originalEmail = email.getText().toString();
+
+
+
+
+        phoneNumber.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                phoneNumber.setVisibility(View.GONE);
+                editphoneNumber.setVisibility(View.VISIBLE);
+                editphoneNumber.setText("");
+
+
+                //onbackpressed, go to prev number that existed
+
+                editphoneNumber.setOnKeyListener(new View.OnKeyListener() {
+
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        changedPhoneNumber = editphoneNumber.getText().toString();
+
+                        // If the event is a key-down event on the "enter" button
+                        if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                                (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+                            if ((changedPhoneNumber.isEmpty()) || (changedPhoneNumber.length()!=10)){
+
+                                //check if digits
+
+
+                                phoneNumber.setText(originalPhoneNumber);
+
+                                phoneNumber.setVisibility(View.VISIBLE);
+                                editphoneNumber.setVisibility(View.GONE);
+
+
+                            }
+                            else{
+                                phoneNumber.setText(changedPhoneNumber);
+
+                                phoneNumber.setVisibility(View.VISIBLE);
+                                editphoneNumber.setVisibility(View.GONE);
+
+                            }
+                            return false;
+
+                        }
+                        return false;
+
+                        }
+            });
+                return false;
+            }
+    });
+
+
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
@@ -38,6 +110,30 @@ public class ProfileActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    //check top left back button as well
+    @Override
+    public void onBackPressed() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Would you like to save changes made?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //go to nav drawer
+                        }
+                    });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+
+
 
 
 }
