6a778507c8e6355e6f2c8afc702e735a3a9a6282
Gerardo Gonzalez <jpgrgmgs@gmail.com>
2016-11-13 22:00:40 -0800

    save profile changes to database

--- app/src/main/java/cse110/liveasy/ProfileActivity.java
+++ app/src/main/java/cse110/liveasy/ProfileActivity.java
@@ -1,5 +1,6 @@
 package cse110.liveasy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -7,13 +8,22 @@ import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.telephony.PhoneNumberUtils;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,15 +37,47 @@ import java.util.Map;
 public class ProfileActivity extends AppCompatActivity {
 
     Bundle extras;
+    String originalPhoneNumber;
+    String changedPhoneNumber;
+
+
+
+    // Spinner flags
+    boolean smokingSpinnerFlag = false;
+
+    boolean canEdit = false;
+    boolean changeFlag = false;
+
+    Map<String, Object> userMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
 
         //final View theView = findViewById(R.id.profile_activity);
 
         extras = getIntent().getExtras();
         String memberName = (String) extras.get("memberName");
+        String userName = (String) extras.get("username");
+
+        Button saveButton = (Button) findViewById(R.id.save_button);
+        if(userName.equals(memberName)) {
+            canEdit = true;
+        }
+        else
+        {
+            saveButton.setVisibility(View.GONE);
+        }
+
+        // Initialize Spinners
+        final Spinner smokingspinner = (Spinner) findViewById(R.id.smoking_spinner);
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        smokingspinner.setAdapter(adapter);
+
+
 
         getSupportActionBar().setTitle(memberName);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -49,11 +91,13 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 final Map<String, Object> currentUserMap = (HashMap<String, Object>) dataSnapshot.getValue();
+                userMap = currentUserMap;
 
                 //TODO IMAGES
 
                 TextView phone = (TextView) findViewById(R.id.main_profile_number);
                 String formattedNumber = PhoneNumberUtils.formatNumber((String) currentUserMap.get("phone_number"), "US");
+                originalPhoneNumber = formattedNumber;
                 phone.setText(formattedNumber);
 
 
@@ -71,15 +115,20 @@ public class ProfileActivity extends AppCompatActivity {
                     drinking.setText("I am OK with it");
                 }
                 else {
-                    drinking.setText("I am not OK with it");
+                    drinking.setText("I am NOT ok with it");
                 }
 
-                TextView smoking = (TextView) findViewById(R.id.smoking1);
+                //TextView smoking = (TextView) findViewById(R.id.smoking1);
                 if ((boolean)currentUserMap.get("smoking")) {
-                    smoking.setText("I am OK with it");
+                    smokingspinner.setSelection(adapter.getPosition("I am OK with it"));
+                    smokingSpinnerFlag = true;
+                    //smoking.setText("I am OK with it");
+
                 }
                 else {
-                    smoking.setText("I am not OK with it");
+                    smokingspinner.setSelection(adapter.getPosition("I am NOT ok with it"));
+                    smokingSpinnerFlag = false;
+                    //smoking.setText("I am not OK with it");
                 }
 
                 TextView pets = (TextView) findViewById(R.id.pets1);
@@ -87,7 +136,7 @@ public class ProfileActivity extends AppCompatActivity {
                     pets.setText("I am OK with it");
                 }
                 else {
-                    pets.setText("I am not OK with it");
+                    pets.setText("I am NOT ok with it");
                 }
 
                 TextView guests = (TextView) findViewById(R.id.guests1);
@@ -95,7 +144,7 @@ public class ProfileActivity extends AppCompatActivity {
                     guests.setText("I am OK with it");
                 }
                 else {
-                    guests.setText("I am not OK with it");
+                    guests.setText("I am NOT ok with it");
                 }
 
                 TextView petpeeves = (TextView) findViewById(R.id.pet_peeves);
@@ -118,11 +167,96 @@ public class ProfileActivity extends AppCompatActivity {
             }
         };
         uRef.addListenerForSingleValueEvent(userListener);
+        uRef.removeEventListener(userListener);
+
+        if(canEdit) {
+
+            // Setup listener for phone text view
+            final TextView phoneNumber = (TextView) findViewById(R.id.main_profile_number);
+            final TextView phoneNumberType = (TextView) findViewById(R.id.phone_type);
+            final EditText editphoneNumber = (EditText) findViewById(R.id.phone_edit_text);
+
+            originalPhoneNumber = phoneNumber.getText().toString();
+
+            phoneNumber.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+
+                    phoneNumber.setVisibility(View.GONE);
+                    phoneNumberType.setVisibility(View.GONE);
+                    editphoneNumber.setVisibility(View.VISIBLE);
+                    editphoneNumber.setText("");
 
 
+                    //onbackpressed, go to prev number that existed
 
-        setContentView(R.layout.activity_profile);
+                    editphoneNumber.setOnKeyListener(new View.OnKeyListener() {
 
+                        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                        public boolean onKey(View v, int keyCode, KeyEvent event) {
+                            changedPhoneNumber = editphoneNumber.getText().toString();
+
+
+                            // If the event is a key-down event on the "enter" button
+                            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+
+                                if ((changedPhoneNumber.isEmpty()) || (changedPhoneNumber.length() != 10)) {
+
+                                    //check if digits
+
+
+                                    phoneNumber.setText(originalPhoneNumber);
+
+                                    phoneNumber.setVisibility(View.VISIBLE);
+                                    phoneNumberType.setVisibility(View.VISIBLE);
+                                    editphoneNumber.setVisibility(View.GONE);
+                                } else {
+                                    changedPhoneNumber = PhoneNumberUtils.formatNumber(changedPhoneNumber, "US");
+                                    phoneNumber.setText(changedPhoneNumber);
+                                    changeFlag = true;
+                                    phoneNumber.setVisibility(View.VISIBLE);
+                                    phoneNumberType.setVisibility(View.VISIBLE);
+                                    editphoneNumber.setVisibility(View.GONE);
+                                    originalPhoneNumber = changedPhoneNumber;
+                                }
+                                return false;
+
+                            }
+                            return false;
+
+                        }
+                    });
+                    return false;
+                }
+            });
+
+
+            // Set Spinner Listeners
+            smokingspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String item = parent.getItemAtPosition(position).toString();
+                    if (item.equals("I am OK with it")) {
+                        if (smokingSpinnerFlag == false) {
+                            smokingSpinnerFlag = true;
+                            changeFlag = true;
+                        }
+                    } else {
+                        if (smokingSpinnerFlag == true) {
+                            smokingSpinnerFlag = false;
+                            changeFlag = true;
+                        }
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+        }
 
     }
     @Override
@@ -130,18 +264,121 @@ public class ProfileActivity extends AppCompatActivity {
     {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Intent goBack = new Intent(this, NavDrawerActivity.class);
-                goBack.putExtra("username", (String)extras.get("username"));
-                startActivity(goBack);
-                finish();
-                //NavUtils.navigateUpTo(this, goBack);
+                if(changeFlag == true && canEdit) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("Would you like to save changes made?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    uploadData();
+                                    changeFlag = false;
+                                    Intent goBack = new Intent(ProfileActivity.this, NavDrawerActivity.class);
+                                    goBack.putExtra("username", (String)extras.get("username"));
+                                    startActivity(goBack);
+                                    finish();
+                                    //NavUtils.navigateUpTo(this, goBack);
+
+
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    Intent goBack = new Intent(ProfileActivity.this, NavDrawerActivity.class);
+                                    goBack.putExtra("username", (String)extras.get("username"));
+                                    startActivity(goBack);
+                                    finish();
+                                }
+                            });
+
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+                else
+                {
+                    Intent goBack = new Intent(this, NavDrawerActivity.class);
+                    goBack.putExtra("username", (String)extras.get("username"));
+                    startActivity(goBack);
+                    finish();
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    public void uploadData()
+    {
+        //uploading...
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference().child("users").child(extras.getString("username"));
+        userMap.put("phone_number", originalPhoneNumber);
+        userMap.put("smoking", smokingSpinnerFlag);
+        ref.updateChildren(userMap);
+    }
+
+    public void onSaveChangesPressed(View v) {
+        System.out.println("In method onSaveChangesPressed.");
+        if(changeFlag == true && canEdit) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Would you like to save changes made?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            uploadData();
+                            changeFlag = false;
+                            //finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //finish();
+                        }
+                    });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        else{
+            Toast toast = Toast.makeText(ProfileActivity.this, "No changes have been made.",
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.NO_GRAVITY, 0, 0);
+            toast.show();
+            //finish();
+        }
+
+    }
+
     @Override
     public void onBackPressed(){
+        if(changeFlag == true && canEdit) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Would you like to save changes made?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            uploadData();
+                            changeFlag = false;
+                            Intent goBack = new Intent(ProfileActivity.this, NavDrawerActivity.class);
+                            goBack.putExtra("username", (String) extras.get("username"));
+                            startActivity(goBack);
+                            finish();
 
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent goBack = new Intent(ProfileActivity.this, NavDrawerActivity.class);
+                            goBack.putExtra("username", (String) extras.get("username"));
+                            startActivity(goBack);
+                            finish();
+                        }
+                    });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        else
+        {
+            Intent goBack = new Intent(this, NavDrawerActivity.class);
+            goBack.putExtra("username", (String) extras.get("username"));
+            startActivity(goBack);
+            finish();
+        }
     }
 }
